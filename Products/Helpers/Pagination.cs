7a59a535309f7826201452666b615ce548beafdd
Manar-Nabil <manar.nabil@business-expert.ai>
2025-04-24 20:20:47 +0200

    layout

--- Products/Helpers/Pagination.cs
+++ Products/Helpers/Pagination.cs
@@ -11,7 +11,7 @@ namespace Products.Helpers
             result.CurrentPage = index > 0 ? index : 1;
             result.Size = size > 0 && size < result.TotalRecords ? size : result.TotalRecords;
             result.TotalPages = (int)Math.Ceiling(((decimal)result.TotalRecords / result.Size));
-            var numberOfExcluded = (result.CurrentPage - 1) * size;
+            var numberOfExcluded = (result.CurrentPage - 1) * result.Size;
             result.values = await values.Skip(numberOfExcluded).Take(size).ToListAsync();
             return result;
         }
