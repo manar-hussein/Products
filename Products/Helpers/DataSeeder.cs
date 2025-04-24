73aef39539c78605816f5c40c2a27d714c0fec8d
Manar-Nabil <manar.nabil@business-expert.ai>
2025-04-24 20:08:48 +0200

    Initiate Dataseeding

---
+++ Products/Helpers/DataSeeder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Products.Models;
+
+namespace Products.Helpers
+{
+    public class DataSeeder
+    {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public DataSeeder(RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        private async Task CreateSallerRole()
+        {
+            var sallerRole = new ApplicationRole { Name = "Saller" };
+            await _roleManager.CreateAsync(sallerRole);
+        }
+    }
+}
